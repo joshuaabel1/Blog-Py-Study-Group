c6a43c7acd9983a77483e245273736a13f711c5b
joshu_los1995 <joshu_los1995@outlook.com>
2022-09-15 13:08:09 -0300

    fix login with django

--- render/build.sh
+++ render/build.sh
@@ -7,8 +7,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser
-joshua
-joshu_los1995@outlook.com
-sociales123 
-sociales123
+python manage.py createsuperuser --email=admin@admin.com --noinput
