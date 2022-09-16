c418def9fb10182e64a94e85715d3182eee1095f
joshu_los1995 <joshu_los1995@outlook.com>
2022-09-16 03:43:29 -0300

    change db sqlit3 to postgresql

--- Blog/admin.py
+++ Blog/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from .model.models import Post, Comment
 # Register your models here.
 # en el admin registramos nuestros modelos para poder usarlos desde aqui.
+admin.site.register(Post)
+admin.site.register(Comment)
