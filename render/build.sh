8399fa1f058d0b75d051665a6f3e201489e86035
joshu_los1995 <joshu_los1995@outlook.com>
2022-09-28 03:42:17 -0300

    fix add posts

--- render/build.sh
+++ render/build.sh
@@ -6,6 +6,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations Blog
+python manage.py makemigrations
 python manage.py migrate
 # python manage.py createsuperuser --email=admin1@admin.com --noinput
