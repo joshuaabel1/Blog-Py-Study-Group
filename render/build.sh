995a610763ac9ba02cd56a6695ded942e1c67b33
joshu_los1995 <joshu_los1995@outlook.com>
2022-10-09 01:43:57 -0300

    changes in models

--- render/build.sh
+++ render/build.sh
@@ -6,6 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic
+python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
 # python manage.py createsuperuser --email=admin1@admin.com --noinput
