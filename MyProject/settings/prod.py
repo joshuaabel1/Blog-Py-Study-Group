5c3121c8a65e8957ad26d1147feec610a56f0cde
joshu_los1995 <joshu_los1995@outlook.com>
2022-10-09 05:25:39 -0300

    fix connect with s3

--- MyProject/settings/prod.py
+++ MyProject/settings/prod.py
@@ -13,6 +13,12 @@ DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
@@ -24,15 +30,15 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'MyProject/static'),
+        os.path.join(BASE_DIR, 'static'),
     ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
                                                                                                                                                                                                                                                                                                                                                         
-AWS_DEFAULT_REGION=os.getenv('AWS_DEFAULT_REGION')
+AWS_DEFAULT_REGION='us-west-2'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERIFY = True
-DEFAULT_FILE_STORAGE = 'MyProject.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'MyProject.settings.storage_backends.MediaStorage'
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
